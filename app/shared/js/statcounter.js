6a516bec000534dc8afc4816548dcdf76de724bb
Sinevia <info@sinevia.com>
2019-09-09 20:34:54 +0100

    Create statcounter.js

---
+++ app/shared/js/statcounter.js
@@ -0,0 +1,9 @@
+<!-- Default Statcounter code for Denoserverless.com http://www.denoserverless.com -->
+<script type="text/javascript">
+var sc_project=12096331; 
+var sc_invisible=1; 
+var sc_security="e3650438"; 
+</script>
+<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>
+<noscript><img class="statcounter" src="https://c.statcounter.com/12096331/0/e3650438/1/" alt="" /></noscript>
+<!-- End of Statcounter Code -->
